04adb616bf9286ed62304bbfc7d04b70a2da1838
Decolemos <decolemos0207@gmail.com>
2026-06-26 16:04:45 -0300

    ajustado app para ser instalado em dispositivos

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,36 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'paw.png', 'paw_cat.png'],
+      manifest: {
+        name: "Pet Shop Management",
+        short_name: "Pet Shop Management",
+        start_url: '/',
+        display: 'standalone',      // 🔑 tira a barra do navegador = cara de app
+        background_color: '#cde1f8',
+        theme_color: '#9ab8e0',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/maskable_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',  // ícone adaptável no Android
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
